e59fe8a3b10cd10e020580458cbf746828c05bec
Andreas Bruckmeier <andreas.bruckmeier@neteleven.de>
2023-12-02 22:17:24 +0100

    Utilize iterators

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,47 +1,53 @@
 fn parse_game(line: &str) -> (u32, u32, u32, u32) {
-    let mut red = 0;
-    let mut green = 0;
-    let mut blue = 0;
+    let (mut r, mut g, mut b) = (0, 0, 0);
     let pos_colon = line.find(':').unwrap();
-    let id = line[5..pos_colon].parse().unwrap();
+    let id = line[line.find(' ').unwrap() + 1..pos_colon]
+        .parse()
+        .unwrap();
     for set in line[pos_colon + 2..].split(';') {
         let set = set.trim();
         for cube in set.split(',') {
             let (count, cube) = cube.trim().split_once(' ').unwrap();
             let count = count.parse().unwrap();
             match cube.chars().next() {
-                Some('r') if count > red => red = count,
-                Some('g') if count > green => green = count,
-                Some('b') if count > blue => blue = count,
-                Some(_) => {}
-                _ => panic!(),
+                Some('r') => {
+                    if count > r {
+                        r = count
+                    }
+                }
+                Some('g') => {
+                    if count > g {
+                        g = count
+                    }
+                }
+                Some('b') => {
+                    if count > b {
+                        b = count
+                    }
+                }
+                _ => panic!("Unexcpected cube name"),
             }
         }
     }
-    (id, red, green, blue)
+    (id, r, g, b)
 }
 
-fn part1() {
-    let mut sum = 0;
-    for line in aoc_utils::read_strings("example_part1.txt").map(|l| l.unwrap()) {
-        let game = parse_game(&line);
-        if game.1 <= 12 && game.2 <= 13 && game.3 <= 14 {
-            sum += game.0;
-        }
-    }
-    println!("Sum of game ids {}", sum);
+fn part1() -> u32 {
+    aoc_utils::read_strings("example_part1.txt")
+        .map(|l| parse_game(&l.unwrap()))
+        .filter(|g| g.1 <= 12 && g.2 <= 13 && g.3 <= 14)
+        .map(|g| g.0)
+        .sum()
 }
 
-fn part2() {
-    let mut sum = 0;
-    for line in aoc_utils::read_strings("example_part2.txt").map(|l| l.unwrap()) {
-        let game = parse_game(&line);
-        sum += game.1 * game.2 * game.3;
-    }
-    println!("Sum of game ids {}", sum);
+fn part2() -> u32 {
+    aoc_utils::read_strings("example_part2.txt")
+        .map(|l| parse_game(&l.unwrap()))
+        .map(|g| g.1 * g.2 * g.3)
+        .sum()
 }
 
 fn main() {
-    part1();
-    part2();
+    println!("Solution of part 1 {}", part1());
+    println!("Solution of part 2 {}", part2());
 }
