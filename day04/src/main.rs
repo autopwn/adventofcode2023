e0c6e21a56f3788ef22219eacfe120931d296008
Andreas Bruckmeier <andreas.bruckmeier@neteleven.de>
2023-12-17 14:08:51 +0100

    day04 part2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,25 +1,47 @@
-fn part1() -> u32 {
+fn split_card(card: &str) -> (Vec<&str>, Vec<&str>) {
+    let (winning, holding) = &card[8..].trim().split_once('|').unwrap();
+    let winning: Vec<_> = winning.trim().split_whitespace().collect();
+    let holding: Vec<_> = holding.trim().split_whitespace().collect();
+    (winning, holding)
+}
+
+fn count_matches(card: &str) -> u64 {
+    let (winning, holding) = split_card(card);
+    winning.iter().filter(|&x| holding.contains(&x)).count() as u64
+}
+
+fn part1(cards: &Vec<&str>) -> u64 {
     let mut sum = 0;
-    for line in aoc_utils::read_strings("example_part1.txt").map(|l| l.unwrap()) {
-        let (winning, holding) = &line[8..].trim().split_once('|').unwrap();
-        let winning: Vec<&str> = winning.split_whitespace().collect();
-        let holding: Vec<&str> = holding.split_whitespace().collect();
+    for card in cards {
+        let (winning, holding) = split_card(card);
         let intersection: Vec<_> = winning.iter().filter(|&x| holding.contains(x)).collect();
         sum += intersection.into_iter().fold(1, |acc, _| acc * 2) / 2;
     }
     sum
 }
 
-fn part2() -> u32 {
-    let mut sum = 0;
-    for line in aoc_utils::read_strings("example_part2.txt").map(|l| l.unwrap()) {
-        println!("{}", line);
-        sum += 1;
+fn part2(cards: &Vec<&str>) -> u64 {
+    let mut overall_count: usize = 0;
+    for number in 0..cards.len() {
+        let mut stack: Vec<usize> = Vec::with_capacity(30);
+        let mut count: usize = 0;
+        stack.push(number);
+        while !stack.is_empty() {
+            let card_number = stack.pop().unwrap();
+            count += 1;
+            let match_count = count_matches(cards[card_number]);
+            for card_number in card_number + 1..=(card_number + match_count as usize) {
+                stack.push(card_number);
+            }
+        }
+        overall_count += count;
     }
-    sum
+    overall_count as u64
 }
 
 fn main() {
-    println!("Solution of part 1 {}", part1());
-    println!("Solution of part 2 {}", part2());
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let cards: Vec<_> = input.trim().split("\n").collect();
+    println!("Solution of part 1 {}", part1(&cards));
+    println!("Solution of part 2 {}", part2(&cards));
 }
